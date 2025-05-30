d8378fe655dbaac248726c954c39d0f239f853db
mohitpuri-codes <mohitpuri.goswami@simformsolutions.com>
2025-05-30 16:51:31 +0530

    feat: Add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
+import { ConfigProvider, Layout, Switch, theme } from "antd";
 import AddTodo from "./components/AddTodo";
 import TodoActions from "./components/DisplayTodo";
+import { useState } from "react";
+import "./index.css";
+import { BulbOutlined, CloudOutlined } from "@ant-design/icons";
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
+  function toggleTheme() {
+    setIsDark((prev) => !prev);
+  }
   return (
     <>
-      <AddTodo />
-      <TodoActions />
+      <ConfigProvider
+        theme={{
+          algorithm: isDark ? theme.darkAlgorithm : theme.defaultAlgorithm,
+        }}
+      >
+        <Layout className="layout">
+          <Switch
+            className="btn"
+            checkedChildren={<BulbOutlined />}
+            unCheckedChildren={<CloudOutlined />}
+            defaultChecked
+            onChange={toggleTheme}
+          />
+          <AddTodo />
+          <TodoActions />
+        </Layout>
+      </ConfigProvider>
     </>
   );
 }
