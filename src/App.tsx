c3b2616eedfc3e5719e5f7b9582dd58c2b1a9c1b
mohitpuri-codes <mohitpuri.goswami@simformsolutions.com>
2025-05-30 15:05:50 +0530

    refactor: lower down the refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { type InputRef } from "antd";
-import { useRef } from "react";
 import AddTodo from "./components/AddTodo";
 import TodoActions from "./components/DisplayTodo";
 
 function App() {
-  const inputRef = useRef<InputRef>(null);
-
   return (
     <>
-      <AddTodo ref={inputRef} />
+      <AddTodo />
       <TodoActions />
     </>
   );
--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,17 +1,13 @@
 import { Button, Input, Space, type InputRef } from "antd";
-import type { RefObject } from "react";
+import { useRef } from "react";
 import { useAppDispatch } from "../../../todo ant-d/src/store/hooks/store-hooks";
 import { todoActions } from "../../../todo ant-d/src/store/features/Todos/TodoSlice";
 
-interface AddTodoProps {
-  ref: RefObject<InputRef | null>;
-}
-
-export default function AddTodo({ ref }: AddTodoProps) {
+export default function AddTodo() {
+  const inputRef = useRef<InputRef>(null);
   const dispatch = useAppDispatch();
   function handleAddTodo() {
-    const currentRef = ref.current;
-    console.log(currentRef);
+    const currentRef = inputRef.current;
     if (currentRef && currentRef.input && currentRef.input.value.trim()) {
       dispatch(todoActions.AddTodo({ task: currentRef.input.value }));
       currentRef.input.value = "";
@@ -20,7 +16,11 @@ export default function AddTodo({ ref }: AddTodoProps) {
 
   return (
     <Space.Compact block>
-      <Input ref={ref} style={{ width: "calc(100% - 200px)" }} />
+      <Input
+        ref={inputRef}
+        style={{ width: "calc(100% - 200px)" }}
+        value={inputRef.current?.input?.value}
+      />
       <Button onClick={handleAddTodo} type="primary">
         Submit
       </Button>
